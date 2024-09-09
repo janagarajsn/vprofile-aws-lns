038c99985656de3de41102bfe19ec13285c785f5
Janagaraj Sangamanarayanan <janagarajs@gmail.com>
2024-09-08 22:31:09 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,13 @@
 variable "region" {
   description = "The AWS region to deploy into"
   type        = string
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
 
 variable "availability_zone" {
   description = "The availability zone for the subnet"
   type        = string
-  default     = "us-east-2a"
+  default     = "us-east-1a"
 }
 
 variable "ami_id" {
